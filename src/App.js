63e29469b7fb85cfc4e13df06e85fc4eb76c079c
prv <pavlykrv@gmail.com>
2018-06-07 20:35:45 +0300

    [react-mobx-1] Add bootstrap ui

--- src/App.js
+++ src/App.js
@@ -1,21 +1,23 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import logo from './logo.svg'
+import './App.css'
+import { Grid, Row, Col } from 'react-bootstrap'
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+	render() {
+		return <Grid>
+				<Row>
+					<Col xs={12}>
+                        <span>Header component</span>
+                    </Col>
+				</Row>
+				<Row>
+					<Col xs={12} md={6} xsOffset={3}>
+                        <span>Postlist component</span>
+					</Col>
+				</Row>
+			</Grid>
+	}
 }
 
-export default App;
+export default App
